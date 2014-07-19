0876d75561fc3fa4002d249d4c518a48256a9b1d
Sergii Mertsalov <sergmerts@gmail.com>
2014-07-19 14:33:30 -0700

    date string into numbers refactored

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,49 @@ var nextWeekday = function(day){
 	var currentDay = currentDate.getDay(); 
 	var currentDayAsMonthDay = currentDate.getDate(); 
 	
-	var monthsInNumbers = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 	var daysInNumbers = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 	
 	var inputtedDay = day.charAt(0).toUpperCase() + day.slice(1); 
   
-  for (var i = 0; daysInNumbers.length > i; i++){
-		if (inputtedDay === daysInNumbers[i]) {
-			inputtedDayAsNumber = i;
-		};
-	}
+  var  dateStringToNumber = function(array,string,number) {
+			for (var i = 0; array.length > i; i++){
+				if (string === array[i]) {
+					number = i;
+				};
+		  }
+		return number;
+		}
 
-	var inputtedDayAsNumber; 
-	var nextOccurenceMonthDay = currentDayAsMonthDay - (currentDay - inputtedDayAsNumber) + 7; 
+	var inputtedDayAsNumber = dateStringToNumber(daysInNumbers,inputtedDay,inputtedDayAsNumber); 
+	
+	var nextOccurenceMonthDay = currentDayAsMonthDay - (currentDay - inputtedDayAsNumber) +7; 
 	var nextOccurenceFullDate = "" + (currentMonth + 1) + "/" + nextOccurenceMonthDay + "/" + currentYear;
 	return nextOccurenceFullDate;
+}
+
+var weekdaysInMonth = function(day, month) {
+	var currentDate = new Date(); 
+	var currentYear = currentDate.getFullYear();
+	var currentMonth = currentDate.getMonth(); 
+	var currentDay = currentDate.getDay(); 
+	var currentDayAsMonthDay = currentDate.getDate(); 
+	
+	var monthsInNumbers = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	var daysInNumbers = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	
+	var inputtedDay = day.charAt(0).toUpperCase() + day.slice(1); 
+  var inputtedMonth = month.charAt(0).toUpperCase() + month.slice(1); 
+
+		var  dateStringToNumber = function(array,string,number) {
+			for (var i = 0; array.length > i; i++){
+				if (string === array[i]) {
+					number = i;
+				};
+		  }
+		return number;
+		}
+	
+	var inputtedMonthAsNumber = dateStringToNumber(monthsInNumbers,inputtedMonth,inputtedMonthAsNumber);
+	var inputtedDayAsNumber = dateStringToNumber(daysInNumbers,inputtedDay,inputtedDayAsNumber); 
+
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,12 @@ describe("nextWeekday", function(){
 	it("should return the date of the next occurrence", function(){
 		nextWeekday("Friday").should.equal("7/25/2014");
 	});
+	it("should return the date of a week from today", function(){
+		nextWeekday("Saturday").should.equal("7/26/2014");
+	});
+});
+describe("weekdaysInMonth", function(){
+	it("should return all the dates of the given weekday in the given month", function(){
+		weekdaysInMonth("Friday", "August").should.equal(["8/1/2014","8/8/2014","8/15/2014","8/22/2014","8/29/2014"]);
+	});
 });
