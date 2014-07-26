a5b661f14453f04796aab6bd4678e1529a92f569
Sergii Mertsalov <sergmerts@gmail.com>
2014-07-26 13:24:31 -0700

    print all the dates of that weekday in the given month

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,26 @@ var nextWeekday = function(day){
 
   var currentDate = new Date(); 
 	var currentYear = currentDate.getFullYear(); 
-	var currentMonth = currentDate.getMonth(); 
-	var monthDay = currentDate.getDate(); 
+	var currentMonth = currentDate.getMonth();  
 	var currentDay = currentDate.getDay(); 
 	var daysInNumbers = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 	var inputtedDay = day.charAt(0).toUpperCase() + day.slice(1); 
 	var numberedDay = 0; 
+	
 	for (var i = 0; daysInNumbers.length > i; i++){
 		if (inputtedDay === daysInNumbers[i]) {
 			numberedDay = i;
 		}
   }
+	
 	if (numberedDay <= currentDay) {
 		numberedDay += 7
 	}
-	var nextOccurenceMonthDay = new Date(currentDate.setDate(currentDate.getDate() + (numberedDay - currentDay)));
+	
+	var nextOccurenceMonthDay = new Date(currentDate.setDate(currentDate.getDate() + (numberedDay - currentDay))); console.log(nextOccurenceMonthDay);
 	var nextOccurenceDate = nextOccurenceMonthDay.getDate();
-	var nextOccurenceFullDate = "" + (currentMonth + 1) + "/" + nextOccurenceDate + "/" + currentYear;
-	console.log(nextOccurenceMonthDay);
+	var nextOccurenceMonth = nextOccurenceMonthDay.getMonth();
+	var nextOccurenceFullDate = "" + (nextOccurenceMonth + 1) + "/" + nextOccurenceDate + "/" + currentYear;
 	return nextOccurenceFullDate;
 };
 
@@ -30,23 +32,38 @@ var weekdaysInMonth = function(day, month) {
 	var daysInNumbers = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 	var inputtedDay = day.charAt(0).toUpperCase() + day.slice(1); 
   var inputtedMonth = month.charAt(0).toUpperCase() + month.slice(1); 
-		var  dateStringToNumber = function(array,string,number) {
-			
-			for (var i = 0; array.length > i; i++){
-				if (string === array[i]) {
-					number = i;
+	
+	var numberedDay = 0; 
+  for (var i = 0; daysInNumbers.length > i; i++){
+				if (inputtedDay === daysInNumbers[i]) {
+					numberedDay = i;
 				}
 		  }
-		return number;
-		};
 	
-	var numberedMonth = dateStringToNumber(monthsInNumbers,inputtedMonth,numberedMonth); 
-	var numberedDay = dateStringToNumber(daysInNumbers,inputtedDay,numberedDay); 
-	var newDate = new Date(currentYear,numberedMonth, numberedDay); 
+
+  var numberedMonth = 0; 
+	for (var i = 0; monthsInNumbers.length > i; i++){
+				if (inputtedMonth === monthsInNumbers[i]) {
+					numberedMonth = i;
+				}
+		  }
+
+var newDate = new Date(currentYear,numberedMonth); 
+var newWeekday = newDate.getDay(); 
+
+if (numberedDay <= newWeekday) {
+		numberedDay += 7
+	}
+	
+
+  var nextOccurenceMonthDay = new Date(newDate.setDate(newDate.getDate() + (numberedDay - newWeekday))); 
+  var nextOccurenceDate = nextOccurenceMonthDay.getDate();
 	var newMonthDay = newDate.getDate(); 
+	var newMonth = newDate.getMonth(); 
+	var lastDayOfMonth = new Date(currentYear,numberedMonth + 1, 0).getDate();
   var allDatesInMonth = [];
   
-  while(newMonthDay < 31) {
+  while(newMonthDay <= lastDayOfMonth) {
   	allDatesInMonth.push(newMonthDay);
   	newMonthDay += 7;
   }
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,14 @@
 describe("nextWeekday", function(){
 	it("should return the date of the next occurrence", function(){
-		nextWeekday("Friday").should.equal("7/25/2014");
+		nextWeekday("Friday").should.equal("8/1/2014");
 	});
 	it("should return the date of a week from today", function(){
-		nextWeekday("Saturday").should.equal("7/26/2014");
+		nextWeekday("Saturday").should.equal("8/2/2014");
 	});
 });
 
 describe("weekdaysInMonth", function(){
 	it("should return all the dates of the given weekday in the given month", function(){
-		weekdaysInMonth("Wednesday", "December").should.eql(["12/3/2014","12/10/2014","12/17/2014","12/24/2014","12/31/2014"]);
+		weekdaysInMonth("friday", "october").should.eql(["10/6/2014","10/13/2014","10/20/2014","10/27/2014"]);
 	});
 });
